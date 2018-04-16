fcd35ae0b9c715c72495a5cb5ffc4b7c0a7670f9
Muhammad Zaim <zaimpauzi@outlook.com>
2018-04-16 23:40:01 +0800

    Rename assembly file

--- Barcode.WhatsappAgent/Properties/AssemblyInfo.cs
+++ Barcode.WhatsappAgent/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Barcode.WhatsappAgent")]
+[assembly: AssemblyTitle("Barcode.TelegramAgent")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Barcode.WhatsappAgent")]
+[assembly: AssemblyProduct("Barcode.TelegramAgent")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
